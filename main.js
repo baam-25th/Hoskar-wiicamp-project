0eb1b441ebc15607ba524b89a50720d11c5627e1
unknown <39127luv@gmail.com>
2022-04-28 04:48:57 +0700

    Upload project Hoskar

--- main.js
+++ main.js
@@ -24,3 +24,17 @@ $(".slick-wrapper").slick({
     prevArrow: '<button class="slick-prev slick-arrow"><ion-icon name="chevron-back-outline"></ion-icon></button>',
     nextArrow: '<div class="slick-next slick-arrow"><ion-icon name="chevron-forward-outline"></ion-icon></div>',
 });
+
+var swiper = new Swiper(".mySwiper1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
